bfc28eae19f09cb29be772100f52aa7638673efb
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-07-05 13:22:36 +0300

    Add files via upload

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -12,6 +12,7 @@ from ordersapp.models import Order, OrderItem
 
 class OrderList(ListView):
    model = Order
+
    def get_queryset(self):
        return Order.objects.filter(user=self.request.user, is_active=True)
 
@@ -23,32 +24,18 @@ class OrderCreate(CreateView):
 
    def get_context_data(self, **kwargs):
        data = super().get_context_data(**kwargs)
-       OrderFormSet = inlineformset_factory(
-           Order,
-           OrderItem,
-           form=OrderItemEditForm,
-           extra=1
-       )
+       OrderFormSet = inlineformset_factory(Order,
+                                            OrderItem,
+                                            form=OrderItemEditForm,
+                                            extra=1)
        if self.request.POST:
            formset = OrderFormSet(self.request.POST)
        else:
-           basket_items = list(Basket.objects.filter(user=self.request.user))
-           if len(basket_items):
-               OrderFormSet = inlineformset_factory(
-                   Order,
-                   OrderItem,
-                   form=OrderItemEditForm,
-                   extra=len(basket_items)
-               )
            formset = OrderFormSet()
-           for num, form in enumerate(formset.forms):
-               form.initial['product'] = basket_items[num].product
-               form.initial['quantity'] = basket_items[num].quantity
-           else:
-               formset = OrderFormSet()
 
        data['orderitems'] = formset
        return data
+
 
    def form_valid(self, form):
        context = self.get_context_data()
