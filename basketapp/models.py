9a303c318a32cd0d79e2fa5bb62358ef7a7682d8
valentinafomina <valentina.s.fomina@gmail.com>
2021-07-19 16:49:27 +0300

    Lesson 7 - Cache

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils.functional import cached_property
+
 from mainapp.models import Product
 
 class BasketQuerySet():
@@ -24,24 +26,30 @@ class Basket(models.Model):
         return self.product.price * self.quantity
     
     product_cost = property(_get_product_cost)
-    
-    
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     def _get_total_quantity(self):
-        "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
-        
-    total_quantity = property(_get_total_quantity)
-    
-    
+        # "return total quantity for user"
+        # _items = Basket.objects.filter(user=self.user)
+        # _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        # return _totalquantity
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+
     def _get_total_cost(self):
         "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
-        
-    # total_cost = property(_get_total_cost)
+        # _items = Basket.objects.filter(user=self.user)
+        # _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        # return _totalcost
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
+
+    total_quantity = property(_get_total_quantity)
+    total_cost = property(_get_total_cost)
 
     @staticmethod
     def get_items(user):
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -227,3 +229,17 @@ if DEBUG:
        'debug_toolbar.panels.profiling.ProfilingPanel',
        'template_profiler_panel.panels.template.TemplateProfilerPanel',
    ]
+
+if os.name == 'posix':
+   CACHE_MIDDLEWARE_ALIAS = 'default'
+   CACHE_MIDDLEWARE_SECONDS = 120
+   CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+   CACHES = {
+       'default': {
+           'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+           'LOCATION': '127.0.0.1:11211',
+       }
+   }
+
+LOW_CACHE = True
